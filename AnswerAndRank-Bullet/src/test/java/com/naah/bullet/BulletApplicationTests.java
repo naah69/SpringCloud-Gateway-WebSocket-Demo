a5df4b44ae9a80f1de61dfb4dd7f9373f35220fa
Naah <nayan3480232@163.com>
2018-08-13 16:02:16 +0800

    firsr commit

---
+++ AnswerAndRank-Bullet/src/test/java/com/naah/bullet/BulletApplicationTests.java
@@ -0,0 +1,16 @@
+package com.naah.bullet;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BulletApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
